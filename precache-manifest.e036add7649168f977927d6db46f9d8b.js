91f45326d0d61abddf11cc473d7a5b48eece0a72
davidnmora <davidnmora@gmail.com>
2020-03-30 13:50:40 -0700

    Updates

---
+++ precache-manifest.e036add7649168f977927d6db46f9d8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b477048f13323baefa1ff67b9b52328",
+    "url": "/mapbox-storytelling/index.html"
+  },
+  {
+    "revision": "018d5ef050962ae4a8d0",
+    "url": "/mapbox-storytelling/static/css/main.cb3be852.chunk.css"
+  },
+  {
+    "revision": "310192c8e207e8bff2bc",
+    "url": "/mapbox-storytelling/static/js/2.94e7c646.chunk.js"
+  },
+  {
+    "revision": "018d5ef050962ae4a8d0",
+    "url": "/mapbox-storytelling/static/js/main.326aed46.chunk.js"
+  },
+  {
+    "revision": "cd098532da83e2d082fe",
+    "url": "/mapbox-storytelling/static/js/runtime~main.b298dead.js"
+  }
+]);
